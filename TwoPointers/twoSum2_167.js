9dd6ad3886baddc84f0e3bba4c6c2a9d6323e548
vilas Gannaram <vilasgannaram@gmail.com>
2022-06-27 09:43:54 +0530

    added comments

--- TwoPointers/twoSum2_167.js
+++ TwoPointers/twoSum2_167.js
@@ -1,9 +1,11 @@
 function twoSum(nums, target) {
+    //  Initilizing left and right pointers
     let l = 0,
         r = nums.length - 1
     while (l < r) {
         if (nums[l] + nums[r] > target) r--
         else if (nums[l] + nums[r] < target) l++
+        //  Adding one to left and right pointers, because nums array is one-indexed based.
         else return [l + 1, r + 1]
     }
 }
